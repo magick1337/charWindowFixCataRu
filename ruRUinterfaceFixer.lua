1f3555f651e98d0c3c83c73b3f94223c455b4bb2
magick1337 <59071647+magick1337@users.noreply.github.com>
2024-05-11 15:07:41 +0700

    1.2a rel
    1. A check has been added to compare game builds; in build 54647, the character window was corrected by the developers.

--- ruRUinterfaceFixer.lua
+++ ruRUinterfaceFixer.lua
@@ -1,25 +1,30 @@
 if GetLocale() == "ruRU" then
-   if not BULNFormatFix then
-      BULNFormatFix = BreakUpLargeNumbers;
-      BreakUpLargeNumbers = function(x)
-         if x and type(x) == "string" then
-            if string.find(x, ",", 1, true) then
-               local fix = string.gsub(x, ",", ".")
-               x = tonumber(fix)
-            else
-               x = tonumber(x)
+   local currBuildString = select(2, GetBuildInfo())
+   local currBuildNumber = tonumber(currBuildString)
+   if currBuildNumber > 54647 then
+      if not BULNFormatFix then
+         BULNFormatFix = BreakUpLargeNumbers;
+         BreakUpLargeNumbers = function(x)
+            if x and type(x) == "string" then
+               if string.find(x, ",", 1, true) then
+                  local fix = string.gsub(x, ",", ".")
+                  x = tonumber(fix)
+               else
+                  x = tonumber(x)
+               end;
             end;
-         end;
+
          if UnitClassBase("player") == "ROGUE" or UnitClassBase("player") == "DRUID" and GetShapeshiftForm() == 3 then
             return BULNFormatFix(x);
          else
             return x;
          end;
-
+         
       end;
    end;
-   print("Исправленное окно персонажа загружено.|n", "charWindowFixCataRu");
-
+      print("Исправленное окно персонажа загружено.|n", "charWindowFixCataRu");
+   end;
+   
    if not SpecializationSpecName and not PetJournalHealPetButtonSpellName then
       print("Применено временное исправление окна коллекции, а так же окна талантов другого игрока.|n", "charWindowFixCataRu")
       CreateFrame("Frame"):CreateFontString("PetJournalHealPetButtonSpellName");
